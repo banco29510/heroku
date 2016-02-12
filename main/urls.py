a33153f9ad5e53e6989117eee1ef11148a8734ac
banco29510 <banco29510@gmail.com>
2016-02-12 08:39:18 +0000

    12/02/2016

--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,6 @@ from . import views
 urlpatterns = [
     
     url(r'^cgu$', views.cgu, name="main-cgu"),
-    url(r'^login$', views.MyLogin, name="main-login"),
-    url(r'^logout$', views.MyLogout, name="main-logout"),
-    url(r'^register$', views.MyRegistration, name="main-register"),
     url(r'^profile$', views.profile, name="main-profile"),
     
 ]
